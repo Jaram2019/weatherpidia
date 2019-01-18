056cf69171b67ea4abf87fdb545c0eee06b9b97d
JmirY <yongseokj3@naver.com>
2019-01-18 19:03:32 +0900

    슬랙을 통해 메세지를 출력하도록 변경

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+import slckbot
 
 #from slckbot import slack_notify
 
@@ -30,7 +31,7 @@ rain = driver.find_element_by_id("wob_pp").text
 output = "지금 " + location + "의 기온은 " + degree + "도, 최저/최고 기온은 " + low + "/" + high + "도 이고, 강수확률은 " + rain + " 입니다."
 if int(rain[0]) > 50:
     output = output + "\n우산을 챙기시는게 좋을 거에요."
-print(output)
+slack_notify(output, '#random', username="날씨봇")
 
 #slack_message = "지금 " + location + "은 " + degree + "입니다"
 #slack_notify(slack_message, '#random', username="날씨봇")
