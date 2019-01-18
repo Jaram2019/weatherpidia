68cbb673c78b4990e67d09c69f96c832357ec4ee
JmirY <yongseokj3@naver.com>
2019-01-18 18:40:19 +0900

    출력 메세지 생성 - 슬랙과 연동되게끔 수정하면 됨.

--- main.py
+++ main.py
@@ -23,10 +23,14 @@ searchbox.submit()
 driver.implicitly_wait(3)
 degree = driver.find_element_by_id("wob_tm").text
 location = driver.find_element_by_id("wob_loc").text
+high = driver.find_element_by_xpath('//*[@id="wob_dp"]/div[1]/div[3]/div[1]/span[1]').text
+low = driver.find_element_by_xpath('//*[@id="wob_dp"]/div[1]/div[3]/div[2]/span[1]').text
 rain = driver.find_element_by_id("wob_pp").text
-print("지금", location, "의 기온은", degree, "도, 강수확률은", rain, "입니다.")
-if int(rain[0]) > 49:
-    print("우산을 챙기시는게 좋을 거에요.")
+# 출력 메세지
+output = "지금 " + location + "의 기온은 " + degree + "도, 최저/최고 기온은 " + low + "/" + high + "도 이고, 강수확률은 " + rain + " 입니다."
+if int(rain[0]) > 50:
+    output = output + "\n우산을 챙기시는게 좋을 거에요."
+print(output)
 
 #slack_message = "지금 " + location + "은 " + degree + "입니다"
 #slack_notify(slack_message, '#random', username="날씨봇")
