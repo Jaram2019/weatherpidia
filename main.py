66c1fa6c24916cf6050159dc7d587227da81c1e6
Yeonggyun.Kim <kygha7205@naver.com>
2019-01-18 14:27:27 +0900

    selenium crawling test
    Signed-off-by: Yeonggyun.Kim <kygha7205@naver.com>

---
+++ main.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+
+path = "./chromedriver.exe"
+
+driver = webdriver.Chrome(path)
+driver.get('http://www.google.com')
+driver.implicitly_wait(2)
+searchbox = driver.find_element_by_name("q")
+searchbox.send_keys("날씨")
+
+searchbox.submit()
+driver.implicitly_wait(3)
+degree = driver.find_element_by_id("wob_tm").text
+location = driver.find_element_by_id("wob_loc").text
+print("지금", location, "은", degree, "도 입니다.")
