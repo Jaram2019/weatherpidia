d34a641509faa1f357d7987a1bed20cf1b2335e3
Yeonggyun.Kim <kygha7205@naver.com>
2019-01-19 13:48:06 +0900

    resolve conflict

---
+++ crawler.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+
+path = "./chromedriver.exe"
+
+# Headless Mode 옵션 추가
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
+options.add_argument('window-size=1920x1080')
+options.add_argument("disable-gpu")
+
+
+def weather_crawling(input_location):   # input_location: 유저가 입력한 지역
+    driver = webdriver.Chrome(path, chrome_options=options)
+    driver.get('http://www.google.com')
+    driver.implicitly_wait(2)
+    searchbox = driver.find_element_by_name("q")
+    searchbox.send_keys(input_location + "날씨")
+
+    searchbox.submit()
+    driver.implicitly_wait(3)
+    degree = driver.find_element_by_id("wob_tm").text   # 현재 기온
+    location = driver.find_element_by_id("wob_loc").text    # 지역
+    high = driver.find_element_by_xpath('//*[@id="wob_dp"]/div[1]/div[3]/div[1]/span[1]').text  # 최고 기온
+    low = driver.find_element_by_xpath('//*[@id="wob_dp"]/div[1]/div[3]/div[2]/span[1]').text   # 최저 기온
+    rain = driver.find_element_by_id("wob_pp").text     # 강수확률
+
+    # 출력 메세지
+    output = "지금 " + location + "의 기온은 " + degree + "도, 최저/최고 기온은 " + low + "/" + high + "도 이고, 강수확률은 " + rain + " 입니다."
+    if int(rain[0]) > 50:
+        output = output + "\n우산을 챙기시는게 좋을 거에요."
+
+#slack_message = "지금 " + location + "은 " + degree + "입니다"
+#slack_notify(slack_message, '#random', username="날씨봇")
--- slckbot.py
+++ slckbot.py
@@ -1,12 +1,65 @@
-import os
+import json
 
+
+import websocket
 from slacker import Slacker
 
-OAuth = os.environ["SLACK_OAUTH"]
 BotUserOauth = os.environ["BOT_USER_OAUTH"]
 
 
-def slack_notify(text=None, channel='random', username="알림봇", attachments=None):
-    token = BotUserOauth
-    slack = Slacker(token)
+token = BotUserOauth
+slack = Slacker(token)
+response = slack.rtm.start()
+sock_endpoint = response.body['url']
+ws = websocket.create_connection(sock_endpoint)
+
+# websocket Processing Part
+def run():
+    while True:
+        try:
+            msg = json.loads(ws.recv())
+            print(msg)
+            if 'text' in msg.keys():
+                slack.chat.post_message(msg['channel'], msg['text'])
+                # break
+                # return
+        except websocket.WebSocketTimeoutException:
+            ws.send(json.dumps({'type': 'ping'}))
+
+        except websocket.WebSocketConnectionClosedException:
+            print("Connection closed")
+            # break
+            return
+        except Exception as e:
+            print(e)
+            # break
+            return
+
+
+# while True:
+run()
+
+
+# async def execute_bot():
+#     ws = await websockets.connect(sock_endpoint)
+#     while True:
+#         message_json = json.loads(await ws.recv())
+#         # print(message_json['type'])
+#         if 'content' in message_json:
+#             input_message = message_json['content']
+#             print(input_message)
+#             slack.chat.post_message(message_json['channel'],input_message)
+#             continue
+#         else:
+#             print(message_json)
+#
+#
+# loop = asyncio.new_event_loop()
+# asyncio.set_event_loop(loop)
+# asyncio.get_event_loop().run_until_complete(execute_bot())
+# asyncio.get_event_loop().run_forever()
+
+
+def slack_notify(text=None, channel='random', username="Weatherpidia", attachments=None):
+
     slack.chat.post_message(text=text, channel=channel, username=username, attachments=attachments)
